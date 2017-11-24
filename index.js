f7317a7b4bdfb3712df8c45e5dc56316a146fd69
Shubham <shubhamcool8527@gmail.com>
2017-11-24 23:32:55 +0530

    adding linting script

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const _ = require('lodash');
 const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
@@ -6,34 +5,34 @@ const exphbs = require('express-handlebars');
 const passport = require('passport');
 const flash = require('connect-flash');
 const app = express();
-const logger = require('./utils/logger')
+const logger = require('./utils/logger');
 
 //Middlewares
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
 app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
+	defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use(session({
-  secret: 'bingo',
-  resave: true,
-  saveUninitialized: true,
+	secret: 'bingo',
+	resave: true,
+	saveUninitialized: true,
 }));
 app.use(flash());
 app.use((req, res, next) => {
-  res.locals.successMsg = req.flash('successMsg');
-  res.locals.errorMsg = req.flash('errorMsg');
-  res.locals.error = req.flash('error');
-  next();
+	res.locals.successMsg = req.flash('successMsg');
+	res.locals.errorMsg = req.flash('errorMsg');
+	res.locals.error = req.flash('error');
+	next();
 });
 require('./routes/routes')(app, passport, logger);
 app.listen(3000, () => {
-  console.log("Express running");
+	logger.info('Express running');
 });
