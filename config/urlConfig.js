067a2993d8ab69b4a8d1c71e393e80175fe99031
Shubham <shubhamcool8527@gmail.com>
2017-12-20 23:05:31 +0530

    added url links

--- config/urlConfig.js
+++ config/urlConfig.js
@@ -1,6 +1,6 @@
 //url for local and development versions
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {url: 'https://node-jwtauth.herokuapp.com'};
+    module.exports = {url: 'https://floating-headland-47675.herokuapp.com'};
   } else {
     module.exports = {url: 'http://localhost:3000'};
   }
